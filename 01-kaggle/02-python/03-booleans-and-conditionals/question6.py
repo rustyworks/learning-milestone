cdaebf98dd9818f416d9ece3a1d73d8720c02a95
Tristanto <tristanto.kurniawan@gmail.com>
2024-02-03 15:15:18 +0700

    Add solution for booleans and conditional question 6 (using boolean as arithmetic operation)

---
+++ 01-kaggle/02-python/03-booleans-and-conditionals/question6.py
@@ -0,0 +1,8 @@
+def exactly_one_topping(ketchup, mustard, onion):
+    """Return whether the customer wants exactly one of the three available toppings
+    on their hot dog.
+    """
+    return ketchup + mustard + onion == 1
+
+# Check your answer
+q6.check()
